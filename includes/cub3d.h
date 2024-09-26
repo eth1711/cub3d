23709e4a3b10325b8253724d90b4c220e3bfc297
ghostyghast <antoinethebestgamer@gmail.com>
2024-09-26 19:01:03 +0800

    wip: writing out data setup, added main data struct and vector struct to header

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 13:28:52 by amaligno          #+#    #+#             */
-/*   Updated: 2024/09/25 17:37:58 by amaligno         ###   ########.fr       */
+/*   Updated: 2024/09/26 18:48:00 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,25 @@
 # include "libft.h"
 # include "math.h"
 # include "mlx.h"
+
+# define WIN_HEIGHT 1240
+# define WIN_WIDTH 512
+
+enum {
+	ON_DESTROY = 17
+};
+
+typedef struct s_vector
+{
+	int	x;
+	int	y;
+}	t_vector;
+
+typedef struct s_data
+{
+	void		*mlx;
+	void		*window;
+	t_vector	player;
+}	t_data;
 
 #endif
--- src/main/cub3d.c
+++ src/main/cub3d.c
@@ -6,15 +6,43 @@
 /*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 15:13:00 by amaligno          #+#    #+#             */
-/*   Updated: 2024/09/26 16:20:05 by amaligno         ###   ########.fr       */
+/*   Updated: 2024/09/26 18:58:07 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include <stdio.h>
 
+int	on_destroy(void *param)
+{
+	(void)param;
+	exit(0);
+}
+
+void	init(t_data *data)
+{
+	data->player.x = 300;
+	data->player.y = 300;
+	data->mlx = mlx_init();
+	data->window = mlx_new_window(data->mlx, WIN_WIDTH, WIN_HEIGHT, "cub3d");
+	mlx_hook(data->window, ON_DESTROY, 0, on_destroy, NULL);
+
+}
+
+void	loop(void *param)
+{
+	t_data	*data;
+
+	data = (t_data *)param;
+}
+
 int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
+	t_data	data;
+	
+	init(&data);
+	mlx_loop_hook(data.mlx, (void *)&data, loop);
+	mlx_loop(data.mlx);
 }
