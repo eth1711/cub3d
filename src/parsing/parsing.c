f5d887a6b395847edd7f574e2d19dc030a2de865
Ethan Lim <ethanlimwl@gmail.com>
2024-10-31 19:18:02 +0800

    parsing: added things to my check_textures

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,15 +6,11 @@
 /*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:03:01 by etlim             #+#    #+#             */
-/*   Updated: 2024/10/30 22:35:24 by etlim            ###   ########.fr       */
+/*   Updated: 2024/10/31 19:09:38 by etlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-void check_path()
-{
-
-}
 
 void check_textures(char *map)
 {
@@ -23,6 +19,7 @@ void check_textures(char *map)
 	bool	*textures;
 	char	**checks;
 	char	*line;
+	char	*path;
 
 	cur_check = 0;
 	textures = (bool[6]){0, 0, 0, 0, 0, 0};
@@ -34,15 +31,19 @@ void check_textures(char *map)
 	while (line)
 	{
 		if (ft_strncmp(line, checks[cur_check], 3) && !textures[cur_check])
-			check_path();
-			textures[cur_check] = 1;
+			path = line + 3;
+			if (open(path, O_RDONLY) > 0 && cur_check < 4)
+			{
+				textures[cur_check] = 1;
+			}
+			if (cur_check >= 4 && path)//needa think of how to check rgb elements
+			{
+				textures[cur_check] = 1;
+			}
 		free(line);
 		line = get_next_line(fd);
-		
 	}
-		
-	
-	
+	close(fd);
 }
 char **str_alloc(char *map, int *lw)
 {
