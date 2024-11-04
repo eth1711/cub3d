38e6f8158df84c7e8e468b72ddfc54df405af37f
eth1711 <ethanlimwl@gmail.com>
2024-11-04 18:12:55 +0800

    parsing:changed the way i check in check_textures, added set_texture_rgb, and added exit_error function to exit.c

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:03:01 by etlim             #+#    #+#             */
-/*   Updated: 2024/11/03 01:45:54 by etlim            ###   ########.fr       */
+/*   Updated: 2024/11/04 17:11:27 by etlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,60 +39,63 @@ int check_rgb(char *line)
 	return 0;
 }
 
-void check_textures(char *map)
+void set_texture_rgb(char *line, char **checks, int cur_check, bool *textures)
 {
-	int		fd;
+	line = line + ft_strlen(checks[cur_check]);
+	if (cur_check < 4 && open(line, O_RDONLY) > 0)
+		textures[cur_check] = 1;
+	else if(cur_check >= 4 && !check_rgb(line))
+		textures[cur_check] = 1;
+}
+
+void check_textures(char *map, int fd)
+{
 	int		cur_check;
 	bool	*textures;
 	char	**checks;
 	char	*line;
 
-	cur_check = 0;
 	textures = (bool[6]){0, 0, 0, 0, 0, 0};
 	checks = (char*[6]){"NO ", "SO ", "WE ", "EA ", "F ", "C "};
-	fd = open(map, O_RDONLY);
-	if (fd < 0)
-		return (NULL);
-	line = get_next_line(fd);
-	while (line)
+	while ((line = get_next_line(fd)) != NULL)
 	{
-		if (ft_strncmp(line, checks[cur_check], ft_strlen(checks[cur_check])) && !textures[cur_check])
-			line = line + ft_strlen(checks[cur_check]);
-			if (open(line, O_RDONLY) > 0 && cur_check < 4)
-				textures[cur_check] = 1;
-			if (cur_check >= 4 && !check_rgb(line))
-				textures[cur_check] = 1;
+		cur_check = 0;
+		while (cur_check < 6)
+		{
+			if (ft_strncmp(line, checks[cur_check], ft_strlen(checks[cur_check])) && !textures[cur_check])
+				set_texture_rgb(line, checks, cur_check, textures);
+			else if (textures[cur_check])
+				exit(-1);//error message;
+		}
 		free(line);
-		line = get_next_line(fd);
 	}
-	close(fd);
 	cur_check = 0;
 	while (cur_check++ < 6)
 		if (!textures[cur_check])
 			return false;
 }
+
 //insert exit function with error code
 
 // char **str_alloc(char *map, int *lw)
 // {
 // 	char	**str;
 // 	char	*line;
-// 	int		fd;
 // 	int		i;
 	
-// 	fd = open(map, O_RDONLY);
-// 	if (fd < 0)
-// 		return (NULL);
 // 	line = get_next_line(fd);
 // }
 
 int parser(char *map)
 {
 	char	**str;
+	char	*line;
 	int		*lw;
+	int		fd;
 
-	check_textures(map);
+	fd = open(map, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	check_textures(map, &fd);
 	str = str_alloc(map, &lw);
-	
-	
 }
