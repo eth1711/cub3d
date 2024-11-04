3504ab1d161674e0b77b441175434dc548c4d479
eth1711 <ethanlimwl@gmail.com>
2024-11-04 15:33:09 +0800

    parsing: added check_rgb

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,11 +6,38 @@
 /*   By: etlim <etlim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 17:03:01 by etlim             #+#    #+#             */
-/*   Updated: 2024/10/31 19:09:38 by etlim            ###   ########.fr       */
+/*   Updated: 2024/11/03 01:45:54 by etlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int check_rgb(char *line)
+{
+	int *values;
+	int cur;
+	int r;
+	int g;
+	int b;
+	
+	values = (int){-1, -1, -1};
+	cur = 0;
+
+	while (*line && cur < 3)
+	{
+		values[cur++] = ft_atoi(line);
+		while (*line && *line != ",")
+			line++;
+		if (*line == ",")
+			line++;
+	}
+	r = values[0];
+	g = values[1];
+	b = values[2];
+	if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255 )
+		return -1;
+	return 0;
+}
 
 void check_textures(char *map)
 {
@@ -19,7 +46,6 @@ void check_textures(char *map)
 	bool	*textures;
 	char	**checks;
 	char	*line;
-	char	*path;
 
 	cur_check = 0;
 	textures = (bool[6]){0, 0, 0, 0, 0, 0};
@@ -30,33 +56,35 @@ void check_textures(char *map)
 	line = get_next_line(fd);
 	while (line)
 	{
-		if (ft_strncmp(line, checks[cur_check], 3) && !textures[cur_check])
-			path = line + 3;
-			if (open(path, O_RDONLY) > 0 && cur_check < 4)
-			{
+		if (ft_strncmp(line, checks[cur_check], ft_strlen(checks[cur_check])) && !textures[cur_check])
+			line = line + ft_strlen(checks[cur_check]);
+			if (open(line, O_RDONLY) > 0 && cur_check < 4)
 				textures[cur_check] = 1;
-			}
-			if (cur_check >= 4 && path)//needa think of how to check rgb elements
-			{
+			if (cur_check >= 4 && !check_rgb(line))
 				textures[cur_check] = 1;
-			}
 		free(line);
 		line = get_next_line(fd);
 	}
 	close(fd);
+	cur_check = 0;
+	while (cur_check++ < 6)
+		if (!textures[cur_check])
+			return false;
 }
-char **str_alloc(char *map, int *lw)
-{
-	char	**str;
-	char	*line;
-	int		fd;
-	int		i;
+//insert exit function with error code
+
+// char **str_alloc(char *map, int *lw)
+// {
+// 	char	**str;
+// 	char	*line;
+// 	int		fd;
+// 	int		i;
 	
-	fd = open(map, O_RDONLY);
-	if (fd < 0)
-		return (NULL);
-	line = get_next_line(fd);
-}
+// 	fd = open(map, O_RDONLY);
+// 	if (fd < 0)
+// 		return (NULL);
+// 	line = get_next_line(fd);
+// }
 
 int parser(char *map)
 {
