c2b4d3b26d85d6da355b527293269c5394a45d33
ghostyghast <antoinethebestgamer@gmail.com>
2024-11-29 15:56:32 +0800

    fix: passed map size to draw rays 2d

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 15:13:00 by amaligno          #+#    #+#             */
-/*   Updated: 2024/11/28 18:35:10 by amaligno         ###   ########.fr       */
+/*   Updated: 2024/11/29 15:54:56 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int	loop(void *param)
 	{
 		draw_map(&data->image, data->map);
 		draw_player(&data->image, data->player, data->map.wall_size);
-		draw_rays_2d(&data->image, rays);
+		draw_rays_2d(&data->image, rays, data->map.wall_size);
 	}
 	mlx_put_image_to_window(data->mlx, data->window, data->image.image, 0, 0);
 	return (0);
