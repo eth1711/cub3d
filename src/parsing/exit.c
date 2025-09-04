d6dc3974d02d734989a7537d09f99f1fbe1fe061
ghostyghast <antoinethebestgamer@gmail.com>
2025-09-04 16:30:11 +0800

    chore: added newline to exit error

--- src/parsing/exit.c
+++ src/parsing/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pringles <pringles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 22:11:33 by etlim             #+#    #+#             */
-/*   Updated: 2024/12/12 19:39:29 by amaligno         ###   ########.fr       */
+/*   Updated: 2025/09/04 15:22:08 by pringles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@ void	exit_error(char *str)
 {
 	ft_putstr_fd("Error\n", 2);
 	ft_putstr_fd(str, 2);
+	ft_putchar_fd('\n', 2);
 	exit(1);
 }
